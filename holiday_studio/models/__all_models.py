79b0df1203be03ce6881f466721bada704157067
mashasodnomik <91768847+mashasodnomik@users.noreply.github.com>
2022-04-14 01:34:54 +0500

    Delete holiday_studio directory

--- holiday_studio/models/__all_models.py
+++
@@ -1,7 +0,0 @@
-from .client import Client
-from .employee import Employee
-from .order import Order
-from .position import Position
-from .client_order import ClientOrder
-from .employee_order import EmployeeOrder
-from .employee_position import EmployeePosition
--- holiday_studio/server/routers/__init__.py
+++
@@ -1,4 +0,0 @@
-from .employee_router import router as employee_router
-from .order_router import router as order_router
-from .order_router import router as order_router
-from .page_router import router as page_router
